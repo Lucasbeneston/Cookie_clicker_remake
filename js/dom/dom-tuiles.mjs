438bcc51bd582d8eb0260d2c2c953aabbeb51db3
Lucas Beneston <benestonlucas@gmail.com>
2020-05-02 14:01:15 +0200

    Création tuiles

--- js/dom/dom-tuiles.mjs
+++ js/dom/dom-tuiles.mjs
@@ -1,32 +1,39 @@
-export default function updateTuile(){
-    // Créer une div parent avec un id "building-cursor" et une class "locked disabled
-    let buildingTuile = document.createElement('div');
-    buildingTuile.id = 'building-cursor';
-    buildingTuile.classList = 'locked disabled';
-    console.log(buildingTuile);
+import {buildings} from "../../js/data.mjs"
 
-    // Créer une div enfant avec la class"icon"
-    let tuileIcon = document.createElement('div');
-    tuileIcon.classList = 'icon';
-    buildingTuile.appendChild(tuileIcon)
+const buildingsDiv = document.getElementById('buildings');
 
-    // Créer une div enfant avec la class "name"
-    let tuileName = document.createElement('div');
-    tuileName.classList = 'name';
-    tuileName.innerHTML = 'Cursor';
-    buildingTuile.appendChild(tuileName);
+export const updateTuile = () => {
+    
+    for (let i = 0; i < buildings.length - 3; i++){
+        // Créer une div parent avec un id "building-cursor" et une class "locked disabled
+        let buildingTuile = document.createElement('div');
+        buildingTuile.id = `building-${buildings[i].name.toLowerCase()}`;
+        buildingTuile.classList = 'locked disabled';
+        buildingsDiv.appendChild(buildingTuile);
 
-    // Créer une div enfant avec la class "cost"
-    let tuileCost = document.createElement('div');
-    tuileCost.classList = 'cost';
-    tuileCost.innerHTML = 15;
-    buildingTuile.appendChild(tuileCost);
+        // Créer une div enfant avec la class"icon"
+        let tuileIcon = document.createElement('div');
+        tuileIcon.className = 'icon';
+        buildingTuile.appendChild(tuileIcon);
 
-    // Créer une div enfant avec la class "number"
-    let tuileNumber = document.createElement('div');
-    tuileNumber.classList = 'number';
-    buildingTuile.appendChild(tuileNumber);
+        // Créer une div enfant avec la class "name"
+        let tuileName = document.createElement('div');
+        tuileName.className = 'name';
+        tuileName.innerHTML = buildings[i].name;
+        buildingTuile.appendChild(tuileName);
 
-    // Ajouter la div "buildingTuile" à la div "buildings"
-    buildings.appendChild(buildingTuile);
+        // Créer une div enfant avec la class "cost"
+        let tuileCost = document.createElement('div');
+        tuileCost.className = 'cost';
+        tuileCost.innerHTML = buildings[i].cost;
+        buildingTuile.appendChild(tuileCost);
+
+        // Créer une div enfant avec la class "number"
+        let tuileNumber = document.createElement('div');
+        tuileNumber.className = 'number';
+        buildingTuile.appendChild(tuileNumber);   
+    }
 }
+
+console.log(buildings);
+console.log(buildingsDiv);
--- js/main.mjs
+++ js/main.mjs
@@ -1,29 +1,9 @@
 import Bakery from "./classes/bakery.mjs"; 
-import Building from "./classes/building.mjs";
+// import Building from "./classes/building.mjs";
+import {updateBakery} from "./dom/dom-bakery.mjs";
+import {updateTuile} from "./dom/dom-tuiles.mjs";
 
-// Importer la fonction "updateBakery(element)"
-import updateBakery from "./dom/dom-bakery.mjs";
 const newBakery = new Bakery();
 updateBakery(newBakery);
 
-// Importer la fonction "updateTuile()"
-import updateTuile from "./dom/dom-tuiles.mjs";
-const newBuilding = new Building({name, description, cookiesPerSecond, cost}, 1);
-console.log(newBuilding);
-updateTuile(newBuilding);
-
-
-// // Pour séléctionner le nom de notre Bakerie
-// let bakeryName = document.querySelector('#bakery h2');
-// console.log('Nom de notre Bakery : ' + bakeryName);
-// bakeryName.textContent = newBakery.name;
-
-// // Pour séléctionner la valeur "?" de "cookies"
-// let cookiesStock = document.querySelector('#cookiesStock span')
-// console.log('La valeur du stock de cookies : ' + cookiesStock);
-// cookiesStock.textContent = newBakery.cookies;
-
-// // Pour séléctionner la valeur "?" de "cookies per second :"
-// let cookiesPerSecond = document.querySelector('#cookiesPerSecond span')
-// console.log('La valeur de cookies per second : ' + cookiesPerSecond);
-// cookiesPerSecond.textContent = newBakery.coockiesPerSecond;
+updateTuile();
