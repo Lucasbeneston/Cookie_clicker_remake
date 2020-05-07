7353d1efb8a63bafe4848ef6fb2231dea59d8404
Lucas Beneston <benestonlucas@gmail.com>
2020-05-07 09:34:42 +0200

    Ajout commentaire

--- js/dom/dom-tuiles.mjs
+++ js/dom/dom-tuiles.mjs
@@ -1,17 +1,27 @@
+// Permet d'importer les données du fichier "data.mjs" avec les valeurs de cost, name, etc.
 import {buildings} from "../../js/data.mjs"
 
+// Permet de cibler la div avec l'id "#buildings" qui se trouve dans notre HTML
 let buildingsDiv = document.getElementById('buildings');
 
+// Fonction qui permet de générer un entier aléatoirement dans un interval
 function entierAleatoire(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
    }
 
+
 export const updateTuile = () => {
 
+    // La boucle "for" permet de lier automatiquement les "valeurs" des tuiles (name, cost, icon, etc.) ...
+    // ... présent dans "data.mjs" aux différentes tuiles que nous créons
     for (let i = 0; i < buildings.length; i++){
-        // Créer une div parent avec un id "building-cursor" et une class "locked disabled
+
+        // Créer une div parent avec un id "building-[i]" et une class "locked disabled
         let buildingTuile = document.createElement('div');
-        buildingTuile.id = `building-${buildings[i].name.toLowerCase()}`;
+        buildingTuile.id = `building-${buildings[i].name.toLowerCase()}`; // toLowerCase() = transforme le texte en minuscule 
+        // [0] = `building-cursor.toLowerCase()}`
+        // [1] = `building-grandma.toLowerCase()}`
+        // ... 
         buildingTuile.classList = 'locked disabled';
         buildingsDiv.appendChild(buildingTuile);
 
@@ -37,9 +47,14 @@ export const updateTuile = () => {
         tuileNumber.classList = 'number';
         buildingTuile.appendChild(tuileNumber); 
         
+
+        // console.log(buildings.length) = 5
+        // CONDITION IF : Si i est inférieur à buildings.length - 3 (donc 2)
         if (i < buildings.length - 3){
+            //ALORS affiche les deux premières tuiles (grâce à "flex")
             buildingTuile.style.display = "flex"
         } else {
+            // SINON cache les tuiles suivantes (grâce à "none")
             buildingTuile.style.display = "none"
         }
     }
@@ -48,22 +63,32 @@ export const updateTuile = () => {
 
 export const mecanismeTuiles = (monObjet) => {
 
+    // Permet de parcourir le tableau des tuiles
     for (let i = 0; i < buildings.length; i++){
     
+        // CONDITION IF : Si le stock de cookies est >= au cost (ex : 15)
         if (monObjet.cookies >= buildings[i].cost){
+
+            // let tuiles > cible la 1ere tuile (la tuile débloqué)
             let tuiles = document.getElementById(`building-${buildings[i].name.toLowerCase()}`);
+
+            // let tuilesUnlocked > cible la 2eme tuile (après la tuile débloqué)
             let tuileUnlocked = document.getElementById(`building-${buildings[i+1].name.toLowerCase()}`);
+
+            // let tuileAfter > cible la 3eme tuile (après la tuile débloqué)
             let tuileAfter = document.getElementById(`building-${buildings[i+2].name.toLowerCase()}`);
 
-            tuiles.classList.remove('locked')
-            tuiles.classList.add('unlocked')
+            // CONDITION IF (vraie) ALORS Active et débloque la 1ere tuile "tuiles"
+            tuiles.classList.remove('locked') // Retire la class .locked (silhouette de l’icône est affichée)
+            tuiles.classList.add('unlocked') // Ajoute la class .unlocked (l’icône originale et en couleur est affichée)
+            tuiles.classList.remove('disabled') // Retire la class .disabled (l’image de fond de la tuile est assombrie + cost en corail)
+            tuiles.classList.add('enabled') // Ajoute la class .enabled (le texte cost est de couleur verte)
 
-            tuiles.classList.remove('disabled')
-            tuiles.classList.add('enabled')
+            // CONDITION IF (vraie) ALORS Débloque la 2eme tuile "tuileUnlocked"
+            tuileUnlocked.classList.remove('locked') // Retire la class .locked (silhouette de l’icône est affichée)
+            tuileUnlocked.classList.add('unlocked') // Ajoute la class .unlocked (l’icône originale et en couleur est affichée)
 
-            tuileUnlocked.classList.remove('locked')
-            tuileUnlocked.classList.add('unlocked')
-
+            // CONDITION IF (vraie) ALORS fait apparaitre la 3eme tuile "tuileAfter"
             tuileAfter.style.display = "flex"
         }
     }
@@ -87,6 +112,7 @@ export const buyTuiles = (monObjet) => {
         tuileNumber.innerHTML = monObjet.number
     }));
 }
+
 
 const audioBuy = () => {
         // Créer une balise "audio"
